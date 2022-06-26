3461a704408f724fc7c4c86f2c11411888ffdc47
pravin desai <pravindesai100@gmail.com>
2022-06-26 12:28:39 +0530

    Search coin feature added

--- app/src/main/java/com/pravin/cryptocompose/ui/coin_list/CoinListScreen.kt
+++ app/src/main/java/com/pravin/cryptocompose/ui/coin_list/CoinListScreen.kt
@@ -6,52 +6,66 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.pravin.cryptocompose.ui.coin_list.CoinListViewModel
 import com.pravin.cryptocompose.ui.screens.Screen
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CoinListScreen(
     navController: NavController,
     viewModel: CoinListViewModel = hiltViewModel()
     ) {
     val state = viewModel.coinListState.value
-
-    Box(modifier = Modifier.fillMaxSize()){
-         LazyColumn(modifier = Modifier.fillMaxSize()){
-             items( state.coins ){coin->
-                 CoinListItem(coin = coin,
-                     onItemClick = {
-                         navController.navigate(Screen.CoinDetailsScreen.route+"/${coin.id}")
-                    }
-                 )
-             }
-         }
-        if (state.error.isNotBlank()){
-            Text(text = state.error,
-                color = MaterialTheme.colorScheme.error,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp)
-                .align(Alignment.Center))
-        }
-
-        if (state.isLoading){
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
-        }
+    
+    val textValue:MutableState<String> = remember {
+        mutableStateOf("")
     }
 
+    Scaffold(
+        topBar = { SearchView(textValue = textValue) },
+        content = { contentPadding->
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(contentPadding)){
+                LazyColumn(modifier = Modifier.fillMaxSize()){
+             val filteredList = state.coins.filter { it.name?.contains(textValue.value)?:true }
+
+                    items( filteredList ){coin->
+                        CoinListItem(coin = coin,
+                            onItemClick = {
+                                navController.navigate(Screen.CoinDetailsScreen.route+"/${coin.id}")
+                            }
+                        )
+                    }
+                }
+                if (state.error.isNotBlank()){
+                    Text(text = state.error,
+                        color = MaterialTheme.colorScheme.error,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(20.dp)
+                            .align(Alignment.Center))
+                }
+
+                if (state.isLoading){
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                }
+            }
+        }
+    )
 }
+
+
